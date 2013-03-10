e737ca3f4b37e864f543243a0e122927a18ff305
Daniel Winkler <danthemanwinkler@gmail.com>
2013-03-10 00:34:54 -0600

    still fleshing out browse online levels

--- src/com/danwink/tacticshooter/screens/BrowseOnlineLevelsScreen.java
+++ src/com/danwink/tacticshooter/screens/BrowseOnlineLevelsScreen.java
@@ -3,7 +3,10 @@ package com.danwink.tacticshooter.screens;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 
+import tacticshooter.Slick2DEventMapper;
+
 import com.phyloa.dlib.dui.DButton;
+import com.phyloa.dlib.dui.DPanel;
 import com.phyloa.dlib.dui.DScrollPane;
 import com.phyloa.dlib.dui.DTextBox;
 import com.phyloa.dlib.dui.DUI;
@@ -29,17 +32,23 @@ public class BrowseOnlineLevelsScreen extends DScreen<GameContainer, Graphics> i
 	DButton downloadSort;
 	DButton creatorSort;
 	
+	public void onActivate( GameContainer gc, DScreenHandler<GameContainer, Graphics> dsh )
+	{
+		dui = new DUI( new Slick2DEventMapper( gc.getInput() ) );
+		
+		DPanel panel = new DPanel( gc.getWidth()/2 - 400, gc.getHeight()/2 - 300, 800, 600 );
+		dui.add( panel );
+		
+		scroll = new DScrollPane( 0, 50, 500, 800 );
+		panel.add( scroll );
+	}
+	
 	public void update( GameContainer gc, int delta )
 	{
 		
 	}
 
 	public void render( GameContainer gc, Graphics g )
-	{
-		
-	}
-
-	public void onActivate( GameContainer gc, DScreenHandler<GameContainer, Graphics> dsh )
 	{
 		
 	}
